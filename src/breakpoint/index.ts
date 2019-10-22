dae02434726e0da906de1dbb9c84ee3ad9fdce4d
Jose C Quintas Jr <juniorquintas@gmail.com>
2019-10-22 15:45:30 +0200

    Extract breakpoint types

--- src/breakpoint/index.ts
+++ src/breakpoint/index.ts
@@ -1,31 +1,4 @@
-type FilterProperties<T, P> = { [K in keyof T]: K extends P ? K : never }[keyof T]
-
-type PickEntries<T> = T[FilterProperties<T, 'entries'>]
-
-type BreakpointsInferred<T extends CreateBreakpointParams> = {
-  up: { [P in keyof PickEntries<MergeParams<T>>]: string }
-  down: { [P in keyof PickEntries<MergeParams<T>>]: string }
-}
-
-export interface CreateBreakpointParams {
-  entries?: { [key: string]: number }
-  unit?: string
-  step?: number
-}
-
-export type Breakpoint<T extends { [key: string]: number }> = {
-  up: Breakpoint<T>
-  down: Breakpoint<T>
-}
-
-type Merge<T extends CreateBreakpointParams, Z extends CreateBreakpointParams> = Omit<T, Extract<keyof T, keyof Z>> &
-  Omit<Z, Exclude<keyof Z, keyof T>>
-
-type MergeParams<T extends CreateBreakpointParams = {}> = T['entries'] extends undefined
-  ? typeof defaultBreakpoints
-  : keyof T['entries'] extends undefined
-  ? typeof defaultBreakpoints
-  : Merge<typeof defaultBreakpoints, T>
+import { CreateBreakpointParams, BreakpointsInferred, Breakpoint } from './types'
 
 const defaultBreakpoints = {
   entries: {
@@ -59,3 +32,5 @@ export const createBreakpoint = <O extends CreateBreakpointParams>(options?: O):
 
   return { up, down } as BreakpointsInferred<O>
 }
+
+export { CreateBreakpointParams, Breakpoint }
