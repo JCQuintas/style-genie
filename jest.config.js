e4bf25a119fed3f63f65bce1b786be352c9f840d
JCQuintas <juniorquintas@gmail.com>
2020-11-01 00:22:48 +0100

    update packages

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['<rootDir>/pkg'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/index.{js,jsx,ts,tsx}'],
 }
